98b59815a6ed8c6a154b8e6ade6172767a9da37d
Saranyan.E <saran201496@gmail.com>
2023-09-01 10:06:33 +0530

    created findUserByUsername()

--- user-auth/src/main/java/com/niit/bej/user/auth/repository/UserRepository.java
+++ user-auth/src/main/java/com/niit/bej/user/auth/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package com.niit.bej.user.auth.repository;
 
-public interface UserRepository {
+import com.niit.bej.user.auth.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<String, User> {
+
+    Optional<User> findUserByUsername();
 }
