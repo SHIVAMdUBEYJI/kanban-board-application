926c36a230efabadcdad6f15df9e5bf60efc4e05
Saranyan.E <saran201496@gmail.com>
2023-09-01 09:59:25 +0530

    generated no args and parameterized constructor

--- user-auth/src/main/java/com/niit/bej/user/auth/model/User.java
+++ user-auth/src/main/java/com/niit/bej/user/auth/model/User.java
@@ -3,4 +3,13 @@ package com.niit.bej.user.auth.model;
 public class User {
    private String username;
    private String password;
+
+    public User() {
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
 }
