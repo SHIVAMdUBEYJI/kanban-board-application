4bffaad4706970a78934bc2c56f6904369229bcc
Saranyan.E <saran201496@gmail.com>
2023-09-01 10:07:09 +0530

    added @Repository in UserRepository

--- user-auth/src/main/java/com/niit/bej/user/auth/repository/UserRepository.java
+++ user-auth/src/main/java/com/niit/bej/user/auth/repository/UserRepository.java
@@ -2,9 +2,10 @@ package com.niit.bej.user.auth.repository;
 
 import com.niit.bej.user.auth.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface UserRepository extends JpaRepository<String, User> {
 
     Optional<User> findUserByUsername();
