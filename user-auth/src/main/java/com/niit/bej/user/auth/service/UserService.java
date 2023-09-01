6f5291564c5ddb31f65401d568b445f75a1c17c7
Saranyan.E <saran201496@gmail.com>
2023-09-01 11:06:32 +0530

    added getAllUsers() in UserService

--- user-auth/src/main/java/com/niit/bej/user/auth/service/UserService.java
+++ user-auth/src/main/java/com/niit/bej/user/auth/service/UserService.java
@@ -4,10 +4,14 @@ import com.niit.bej.user.auth.exception.UserAlreadyExistsException;
 import com.niit.bej.user.auth.exception.UserNotFoundException;
 import com.niit.bej.user.auth.model.User;
 
+import java.util.List;
+
 public interface UserService {
 
 User register(User user) throws UserAlreadyExistsException;
 
 boolean login(User user) throws UserNotFoundException;
+
+List<User> getAllUsers() throws UserNotFoundException;
 
 }
