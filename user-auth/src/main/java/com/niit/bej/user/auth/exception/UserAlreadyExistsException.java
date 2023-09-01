25160b5be70c6078f26468271882f89d7d0e63a8
Saranyan.E <saran201496@gmail.com>
2023-09-01 10:27:49 +0530

    created InvalidCredentialsException.java

--- user-auth/src/main/java/com/niit/bej/user/auth/exception/UserAlreadyExistsException.java
+++ user-auth/src/main/java/com/niit/bej/user/auth/exception/UserAlreadyExistsException.java
@@ -1,4 +1,7 @@
 package com.niit.bej.user.auth.exception;
 
 public class UserAlreadyExistsException extends Exception{
+    public UserAlreadyExistsException(String message){
+        super(message);
+    }
 }
