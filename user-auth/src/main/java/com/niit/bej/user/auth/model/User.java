4c6595727cac86e30fa3cd5ea25a59fa906dee69
Saranyan.E <saran201496@gmail.com>
2023-09-01 10:00:20 +0530

    generated getters and setters;equals() and hashcode()

--- user-auth/src/main/java/com/niit/bej/user/auth/model/User.java
+++ user-auth/src/main/java/com/niit/bej/user/auth/model/User.java
@@ -1,5 +1,7 @@
 package com.niit.bej.user.auth.model;
 
+import java.util.Objects;
+
 public class User {
    private String username;
    private String password;
@@ -12,4 +14,32 @@ public class User {
         this.password = password;
     }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(username, user.username) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, password);
+    }
 }
