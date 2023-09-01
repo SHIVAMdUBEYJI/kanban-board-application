44e6dfac42dd4cd6de135c08df2439a6be97c4e2
Saranyan.E <saran201496@gmail.com>
2023-09-01 10:16:54 +0530

    created register() and login() inside the UserService

--- user-auth/src/main/java/com/niit/bej/user/auth/service/UserService.java
+++ user-auth/src/main/java/com/niit/bej/user/auth/service/UserService.java
@@ -1,7 +1,11 @@
 package com.niit.bej.user.auth.service;
 
+import com.niit.bej.user.auth.model.User;
+
 public interface UserService {
 
+User register(User user);
 
+boolean login(User user);
 
 }
