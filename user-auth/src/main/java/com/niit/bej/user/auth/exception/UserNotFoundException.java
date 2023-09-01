faa214c886b46496d862efccdddeeded02af0c0d
Saranyan.E <saran201496@gmail.com>
2023-09-01 10:23:28 +0530

    Create a parameterized constructor and with the super keyword call the parent Exception class

--- user-auth/src/main/java/com/niit/bej/user/auth/exception/UserNotFoundException.java
+++ user-auth/src/main/java/com/niit/bej/user/auth/exception/UserNotFoundException.java
@@ -1,4 +1,7 @@
 package com.niit.bej.user.auth.exception;
 
-public class UserNotFoundException {
+public class UserNotFoundException extends Exception {
+    public UserNotFoundException(String message){
+        super(message);
+    }
 }
