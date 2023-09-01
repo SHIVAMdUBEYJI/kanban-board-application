37429eb5eee7e12bf9ec878179a7aea58a8b4d39
Saranyan.E <saran201496@gmail.com>
2023-09-01 12:27:31 +0530

    added getAllRegisteredUsers() in UserController.java ; added @GetMapping

--- user-auth/src/main/java/com/niit/bej/user/auth/controller/UserController.java
+++ user-auth/src/main/java/com/niit/bej/user/auth/controller/UserController.java
@@ -1,15 +1,17 @@
 package com.niit.bej.user.auth.controller;
 
+import com.niit.bej.user.auth.exception.InvalidCredentialsException;
 import com.niit.bej.user.auth.exception.UserAlreadyExistsException;
+import com.niit.bej.user.auth.exception.UserNotFoundException;
 import com.niit.bej.user.auth.model.User;
 import com.niit.bej.user.auth.service.UserService;
 import com.niit.bej.user.auth.service.security.SecurityTokenGenerator;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/home")
@@ -23,14 +25,35 @@ public class UserController {
     }
 
     @PostMapping("/register")
-    public ResponseEntity<?> register(@RequestBody User user) throws UserAlreadyExistsException {
+    public ResponseEntity<?> register(@RequestBody User user) {
         try {
             User registeredUser = this.userService.register(user);
-            return new ResponseEntity<>(registeredUser, HttpStatus.CREATED);
+            return new ResponseEntity<>(registeredUser + "created", HttpStatus.CREATED);
         } catch (UserAlreadyExistsException exception) {
-            throw new UserAlreadyExistsException(exception.getMessage());
+            return new ResponseEntity<>(exception.getMessage(), HttpStatus.CONFLICT);
         }
     }
 
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody User user) {
+        try {
+            boolean loginUser = this.userService.login(user);
+            if (loginUser) {
+                Map<String, String> jwtTokenMap = securityTokenGenerator.generateToken(user);
+                return new ResponseEntity<>(jwtTokenMap, HttpStatus.OK);
+            } else throw new InvalidCredentialsException("Please check your Username and Password");
+        } catch (InvalidCredentialsException | UserNotFoundException exception) {
+            return new ResponseEntity<>("Please check your credentials", HttpStatus.BAD_REQUEST);
+        }
+    }
 
+    @GetMapping("/all")
+    public ResponseEntity<?> getAllRegisteredUsers() {
+        try {
+            List<User> userList = this.userService.getAllUsers();
+            return new ResponseEntity<>(userList, HttpStatus.OK);
+        } catch (UserNotFoundException exception) {
+            return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
 }
