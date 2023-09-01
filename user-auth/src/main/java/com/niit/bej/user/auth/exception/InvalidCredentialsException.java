ebcd9ad7d31e6bd8e2a5bb336164e466d8090fcb
Saranyan.E <saran201496@gmail.com>
2023-09-01 10:28:59 +0530

    added a parameterized constructor

--- user-auth/src/main/java/com/niit/bej/user/auth/exception/InvalidCredentialsException.java
+++ user-auth/src/main/java/com/niit/bej/user/auth/exception/InvalidCredentialsException.java
@@ -1,4 +1,7 @@
 package com.niit.bej.user.auth.exception;
 
 public class InvalidCredentialsException extends Exception{
+    public InvalidCredentialsException(String message){
+        super(message);
+    }
 }
