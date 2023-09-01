883fde5af0a38956420701c6bba9e4706e2fdb23
Saranyan.E <saran201496@gmail.com>
2023-09-01 11:04:51 +0530

    added register() and login() in UserServiceImpl.java

--- user-auth/src/main/java/com/niit/bej/user/auth/service/UserService.java
+++ user-auth/src/main/java/com/niit/bej/user/auth/service/UserService.java
@@ -1,11 +1,13 @@
 package com.niit.bej.user.auth.service;
 
+import com.niit.bej.user.auth.exception.UserAlreadyExistsException;
+import com.niit.bej.user.auth.exception.UserNotFoundException;
 import com.niit.bej.user.auth.model.User;
 
 public interface UserService {
 
-User register(User user);
+User register(User user) throws UserAlreadyExistsException;
 
-boolean login(User user);
+boolean login(User user) throws UserNotFoundException;
 
 }
--- user-auth/src/main/java/com/niit/bej/user/auth/service/UserServiceImpl.java
+++ user-auth/src/main/java/com/niit/bej/user/auth/service/UserServiceImpl.java
@@ -1,15 +1,40 @@
 package com.niit.bej.user.auth.service;
 
+import com.niit.bej.user.auth.exception.UserAlreadyExistsException;
+import com.niit.bej.user.auth.exception.UserNotFoundException;
 import com.niit.bej.user.auth.model.User;
+import com.niit.bej.user.auth.repository.UserRepository;
+import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+@Service
 public class UserServiceImpl implements UserService {
-    @Override
-    public User register(User user) {
-        return null;
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
     @Override
-    public boolean login(User user) {
-        return false;
+    public User register(User user) throws UserAlreadyExistsException {
+        Optional<User> optionalUser = this.userRepository.findUserByUsername(user.getUsername());
+        if (optionalUser.isPresent()) {
+            throw new UserAlreadyExistsException("username is taken! ");
+        }
+        return this.userRepository.save(user);
+
+    }
+
+    @Override
+    public boolean login(User user) throws UserNotFoundException {
+        Optional<User> optionalUser = this.userRepository.findUserByUsername(user.getUsername());
+        if (optionalUser.isEmpty()) {
+            throw new UserNotFoundException("username not found! ");
+        } else {
+            User userFromDatabase = optionalUser.get();
+            return user.getUsername().equals(userFromDatabase.getUsername()) && user.getPassword().equals(userFromDatabase.getPassword());
+
+        }
     }
 }
