c880a49b364831f86b4450c26f4e1937543f2fd8
Saranyan.E <saran201496@gmail.com>
2023-09-03 12:17:23 +0530

    generated getters and setters ; equals() and hashcode()

--- kanban-board/src/main/java/com/niit/bej/kanban/board/model/Kanban.java
+++ kanban-board/src/main/java/com/niit/bej/kanban/board/model/Kanban.java
@@ -1,5 +1,7 @@
 package com.niit.bej.kanban.board.model;
 
+import java.util.Objects;
+
 public class Kanban {
     private int id;
     private String title;
@@ -12,5 +14,42 @@ public class Kanban {
         this.id = id;
         this.title = title;
         this.tasks = tasks;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Tasks getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(Tasks tasks) {
+        this.tasks = tasks;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Kanban kanban = (Kanban) o;
+        return id == kanban.id && Objects.equals(title, kanban.title) && Objects.equals(tasks, kanban.tasks);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, tasks);
     }
 }
