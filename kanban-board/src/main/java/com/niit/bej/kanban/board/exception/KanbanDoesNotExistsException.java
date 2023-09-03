95e65f165b27c5d9fa709a1860d5419f2ee9f8c9
Saranyan.E <saran201496@gmail.com>
2023-09-03 13:18:05 +0530

    created KanbanDoesNotExistsException.java

---
+++ kanban-board/src/main/java/com/niit/bej/kanban/board/exception/KanbanDoesNotExistsException.java
@@ -0,0 +1,7 @@
+package com.niit.bej.kanban.board.exception;
+
+public class KanbanDoesNotExistsException extends Exception{
+    public KanbanDoesNotExistsException(String message){
+        super(message);
+    }
+}
--- kanban-board/src/main/java/com/niit/bej/kanban/board/service/KanbanService.java
+++ kanban-board/src/main/java/com/niit/bej/kanban/board/service/KanbanService.java
@@ -1,10 +1,14 @@
 package com.niit.bej.kanban.board.service;
 
+import com.niit.bej.kanban.board.exception.TitleNotFoundException;
 import com.niit.bej.kanban.board.model.Kanban;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface KanbanService {
-    Optional<Kanban> getByTitle(String title);
+    Optional<Kanban> getByTitle(String title) throws TitleNotFoundException;
+
+    List<Kanban> getAllKanban();
 
 }
