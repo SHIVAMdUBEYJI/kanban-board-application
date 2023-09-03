1ddd4567dd3a237933e6d1d3a4148bbf358fb0db
Saranyan.E <saran201496@gmail.com>
2023-09-03 12:22:24 +0530

    generated constructors

--- kanban-board/src/main/java/com/niit/bej/kanban/board/model/Cards.java
+++ kanban-board/src/main/java/com/niit/bej/kanban/board/model/Cards.java
@@ -8,4 +8,15 @@ public class Cards {
     private String colors;
     private String members;
     private Date dueDate;
+
+    public Cards() {
+    }
+
+    public Cards(String title, String description, String colors, String members, Date dueDate) {
+        this.title = title;
+        this.description = description;
+        this.colors = colors;
+        this.members = members;
+        this.dueDate = dueDate;
+    }
 }
