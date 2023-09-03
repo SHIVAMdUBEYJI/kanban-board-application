b9208a279300696f824e6ccff0d3018f61b3c268
Saranyan.E <saran201496@gmail.com>
2023-09-03 16:10:55 +0530

    created TaskController.java

--- kanban-board/src/main/java/com/niit/bej/kanban/board/controller/KanbanController.java
+++ kanban-board/src/main/java/com/niit/bej/kanban/board/controller/KanbanController.java
@@ -1,4 +1,17 @@
 package com.niit.bej.kanban.board.controller;
 
+import com.niit.bej.kanban.board.service.KanbanService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
 public class KanbanController {
+
+    private final KanbanService kanbanService;
+
+
+    public KanbanController(KanbanService kanbanService) {
+        this.kanbanService = kanbanService;
+    }
 }
---
+++ kanban-board/src/main/java/com/niit/bej/kanban/board/controller/TaskController.java
@@ -0,0 +1,4 @@
+package com.niit.bej.kanban.board.controller;
+
+public class TaskController {
+}
