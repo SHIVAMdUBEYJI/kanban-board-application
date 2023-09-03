b397507da815c80bc1691d2af7bc96893c1b7085
Saranyan.E <saran201496@gmail.com>
2023-09-03 16:18:39 +0530

    added @PostMapping; created createKanban() in KanbanController.java

--- kanban-board/src/main/java/com/niit/bej/kanban/board/controller/KanbanController.java
+++ kanban-board/src/main/java/com/niit/bej/kanban/board/controller/KanbanController.java
@@ -1,6 +1,12 @@
 package com.niit.bej.kanban.board.controller;
 
+import com.niit.bej.kanban.board.exception.KanbanAlreadyExistsException;
+import com.niit.bej.kanban.board.model.Kanban;
 import com.niit.bej.kanban.board.service.KanbanService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +16,17 @@ public class KanbanController {
 
     private final KanbanService kanbanService;
 
-
     public KanbanController(KanbanService kanbanService) {
         this.kanbanService = kanbanService;
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<?> createKanban(@RequestBody Kanban kanban) {
+        try {
+            Kanban createdkanban = this.kanbanService.saveKanban(kanban);
+            return new ResponseEntity<>(createdkanban, HttpStatus.OK);
+        } catch (KanbanAlreadyExistsException exception) {
+            return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
 }
