5f2d684d424f726355b2a12272b0235d69416340
Saranyan.E <saran201496@gmail.com>
2023-09-03 12:16:21 +0530

    generated the no args and parameterized constructors for Kanban.java

--- kanban-board/src/main/java/com/niit/bej/kanban/board/model/Kanban.java
+++ kanban-board/src/main/java/com/niit/bej/kanban/board/model/Kanban.java
@@ -4,4 +4,13 @@ public class Kanban {
     private int id;
     private String title;
     private Tasks tasks;
+
+    public Kanban() {
+    }
+
+    public Kanban(int id, String title, Tasks tasks) {
+        this.id = id;
+        this.title = title;
+        this.tasks = tasks;
+    }
 }
