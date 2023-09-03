3698a2bbd7f03eaf202edff1bf2e107243dd1962
Saranyan.E <saran201496@gmail.com>
2023-09-03 16:25:19 +0530

    added application.yml

--- kanban-board/src/main/java/com/niit/bej/kanban/board/controller/KanbanController.java
+++ kanban-board/src/main/java/com/niit/bej/kanban/board/controller/KanbanController.java
@@ -1,14 +1,14 @@
 package com.niit.bej.kanban.board.controller;
 
 import com.niit.bej.kanban.board.exception.KanbanAlreadyExistsException;
+import com.niit.bej.kanban.board.exception.KanbanDoesNotExistsException;
 import com.niit.bej.kanban.board.model.Kanban;
 import com.niit.bej.kanban.board.service.KanbanService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -24,9 +24,19 @@ public class KanbanController {
     public ResponseEntity<?> createKanban(@RequestBody Kanban kanban) {
         try {
             Kanban createdkanban = this.kanbanService.saveKanban(kanban);
-            return new ResponseEntity<>(createdkanban, HttpStatus.OK);
+            return new ResponseEntity<>(createdkanban, HttpStatus.CREATED);
         } catch (KanbanAlreadyExistsException exception) {
             return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/getAll")
+    public ResponseEntity<?> getAllKanban() {
+        try {
+            List<Kanban> kanbanList = this.kanbanService.getAllKanban();
+            return new ResponseEntity<>(kanbanList, HttpStatus.OK);
+        } catch (KanbanDoesNotExistsException exception) {
+            return new ResponseEntity<>(exception.getMessage(), HttpStatus.CONFLICT);
         }
     }
 }
