2a207951822a1bc701caa0f17c6ac72b0fca9d5d
Saranyan.E <saran201496@gmail.com>
2023-09-04 14:54:28 +0530

    created Implementation for TasksService

---
+++ kanban-board/src/main/java/com/niit/bej/kanban/board/service/TasksServiceImpl.java
@@ -0,0 +1,33 @@
+package com.niit.bej.kanban.board.service;
+
+import com.niit.bej.kanban.board.model.Tasks;
+
+import java.util.List;
+import java.util.Optional;
+
+public class TasksServiceImpl implements TasksService {
+    @Override
+    public Tasks saveTask(Tasks tasks) {
+        return null;
+    }
+
+    @Override
+    public List<Tasks> getAllTasks() {
+        return null;
+    }
+
+    @Override
+    public Optional<Tasks> getTaskByTitle(String title) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Tasks updateTask(Tasks tasks) {
+        return null;
+    }
+
+    @Override
+    public void deleteTask(Tasks tasks) {
+
+    }
+}
