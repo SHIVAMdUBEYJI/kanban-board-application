18e9d821fc331fbe4baf20920dafc272ac97d4b9
Saranyan.E <saran201496@gmail.com>
2023-09-04 09:59:28 +0530

    added the dependencies for eureka server in pom.xml files

--- api-gateway/src/main/java/com/niit/bej/api/gateway/config/UserAuthRouteConfig.java
+++ api-gateway/src/main/java/com/niit/bej/api/gateway/config/UserAuthRouteConfig.java
@@ -3,6 +3,7 @@ package com.niit.bej.api.gateway.config;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
