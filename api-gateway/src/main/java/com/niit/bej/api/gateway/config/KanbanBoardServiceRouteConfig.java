772f6a0ad0e82e560265e050ee5c1c63dfd47f3b
Saranyan.E <saran201496@gmail.com>
2023-09-04 09:46:20 +0530

    created eureka-server module

--- api-gateway/src/main/java/com/niit/bej/api/gateway/config/KanbanBoardServiceRouteConfig.java
+++ api-gateway/src/main/java/com/niit/bej/api/gateway/config/KanbanBoardServiceRouteConfig.java
@@ -1,4 +1,19 @@
 package com.niit.bej.api.gateway.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@LoadBalancerClient(name = "kanban-board-service")
 public class KanbanBoardServiceRouteConfig {
+
+    @Bean
+    @LoadBalanced
+    public RouteLocator locateKanbanMicroserviceRoutes(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes().route(routePredicate -> routePredicate.path("/api/v1/users/**").uri("lb://kanban-board-service")).build();
+    }
+
 }
