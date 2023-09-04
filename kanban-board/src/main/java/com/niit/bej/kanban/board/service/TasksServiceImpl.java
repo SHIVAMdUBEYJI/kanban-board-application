9c94dc0a1bb22e0c9d6aacbd5910d2250e3df9d2
Saranyan.E <saran201496@gmail.com>
2023-09-04 14:59:43 +0530

    created saveTask() ; getAllTasks()

--- kanban-board/src/main/java/com/niit/bej/kanban/board/service/TasksServiceImpl.java
+++ kanban-board/src/main/java/com/niit/bej/kanban/board/service/TasksServiceImpl.java
@@ -1,24 +1,36 @@
 package com.niit.bej.kanban.board.service;
 
 import com.niit.bej.kanban.board.model.Tasks;
+import com.niit.bej.kanban.board.repository.TasksRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class TasksServiceImpl implements TasksService {
+
+    private final TasksRepository tasksRepository;
+
+    public TasksServiceImpl(TasksRepository tasksRepository) {
+        this.tasksRepository = tasksRepository;
+    }
+
     @Override
     public Tasks saveTask(Tasks tasks) {
-        return null;
+        return this.tasksRepository.save(tasks);
     }
 
     @Override
     public List<Tasks> getAllTasks() {
-        return null;
+        List<Tasks> tasksList = this.tasksRepository.findAll();
+        return tasksList;
     }
 
     @Override
     public Optional<Tasks> getTaskByTitle(String title) {
-        return Optional.empty();
+        Optional<Tasks> optionalTasks = this.tasksRepository.findByTitle(title);
+        return optionalTasks;
     }
 
     @Override
