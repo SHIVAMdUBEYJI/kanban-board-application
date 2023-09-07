8b5f3372f2ceba682cc44e489d5d266d79438424
Saranyan.E <saran201496@gmail.com>
2023-09-07 13:13:29 +0530

    created register-form

--- kanban-board-frontend/src/app/components/register-form/register-form.component.ts
+++ kanban-board-frontend/src/app/components/register-form/register-form.component.ts
@@ -1,36 +1,10 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup} from "@angular/forms";
-import {Router} from "@angular/router";
-import {UserService} from "../../services/user-service";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-register-form',
   templateUrl: './register-form.component.html',
   styleUrls: ['./register-form.component.css']
 })
-export class RegisterFormComponent implements OnInit{
-	registerForm!: FormGroup;
-
-	constructor(private formBuilder: FormBuilder, private router: Router, private userService: UserService) {
-		this.userService = userService;
-	}
-
-	ngOnInit() {
-		this.registerForm = this.formBuilder.group({
-			username: [''],
-			password: ['']
-		})
-	}
-
-	register() {
-		this.userService.registerUser(this.registerForm.value)
-			.subscribe(response => {
-				alert("Registered Successfully");
-				this.registerForm.reset();
-				this.router.navigate(['login'])
-			}, error => {
-				alert("Something went Wrong");
-			})
-	}
+export class RegisterFormComponent {
 
 }
