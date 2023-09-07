7fb9ceb45c737ce624676d27968de81f2076fe33
Saranyan.E <saran201496@gmail.com>
2023-09-07 11:59:32 +0530

    created registerUser() in service

--- kanban-board-frontend/src/app/components/register-form/register-form.component.ts
+++ kanban-board-frontend/src/app/components/register-form/register-form.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup} from "@angular/forms";
 import {Router} from "@angular/router";
+import {UserService} from "../../services/user-service";
 
 @Component({
   selector: 'app-register-form',
--- kanban-board-frontend/src/app/services/user-service.ts
+++ kanban-board-frontend/src/app/services/user-service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@angular/core';
+import {Injectable, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {User} from "../models/user.model";
+import {Observable} from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
-export class UserService {
+export class UserService implements OnInit {
 
-  constructor() { }
+    userURL: string = `http://localhost:3000/users`
+
+    constructor(private httpClient: HttpClient) {
+        this.httpClient = httpClient;
+    }
+
+    ngOnInit(): void {
+    }
+
+    registerUser(user: User): Observable<User> {
+        return this.httpClient.post<User>(this.userURL, user);
+    }
 }
