130f19815c7f7aed3adc8756b38560b42c816e77
Saranyan.E <saran201496@gmail.com>
2023-09-08 09:45:32 +0530

    added bootstrap cdn links

--- kanban-board-frontend/src/app/components/register-form/register-form.component.ts
+++ kanban-board-frontend/src/app/components/register-form/register-form.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {Router} from "@angular/router";
+import {UserService} from "../../services/user-service";
 
 @Component({
-  selector: 'app-register-form',
-  templateUrl: './register-form.component.html',
-  styleUrls: ['./register-form.component.css']
+    selector: 'app-register-form', templateUrl: './register-form.component.html', styleUrls: ['./register-form.component.css']
 })
-export class RegisterFormComponent {
+export class RegisterFormComponent implements OnInit {
+    registerForm!: FormGroup;
+
+    ngOnInit(): void {
+    }
 
 }
