653b9bc3332464818df95565d990d7b58cebe88c
Saranyan.E <saran201496@gmail.com>
2023-09-20 14:52:44 +0530

    added the lombok dependency

--- kanban-board/src/main/java/com/niit/bej/kanban/board/model/KanbanDTO.java
+++ kanban-board/src/main/java/com/niit/bej/kanban/board/model/KanbanDTO.java
@@ -1,5 +1,15 @@
 package com.niit.bej.kanban.board.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class KanbanDTO {
+
     private String title;
 }
