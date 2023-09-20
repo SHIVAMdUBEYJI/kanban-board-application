5aa4e260494aba22599a51e593dafaff0d23612e
Saranyan.E <saran201496@gmail.com>
2023-09-20 15:14:36 +0530

    added apiModelproperty annotation to Task.java

--- kanban-board/src/main/java/com/niit/bej/kanban/board/model/Task.java
+++ kanban-board/src/main/java/com/niit/bej/kanban/board/model/Task.java
@@ -1,9 +1,32 @@
 package com.niit.bej.kanban.board.model;
 
+import io.swagger.annotations.ApiModelProperty;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@Table(name = "task")
 public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @ApiModelProperty(position = 1)
     private Long id;
+
+    @ApiModelProperty(position = 2)
     private String title;
+
+    @ApiModelProperty(position = 3)
     private String description;
+
+    @ApiModelProperty(position = 4)
     private String color;
+
+    @Enumerated(EnumType.STRING)
+    @ApiModelProperty(position = 5)
     private TaskStatus status;
+
 }
