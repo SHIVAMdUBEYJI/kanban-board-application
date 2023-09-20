20f9d8b4e41819ecd7379e1117209c9b99686736
Saranyan.E <saran201496@gmail.com>
2023-09-20 21:27:56 +0530

    injected TaskRepository constructor dependency

--- kanban-board/src/main/java/com/niit/bej/kanban/board/service/TaskServiceImpl.java
+++ kanban-board/src/main/java/com/niit/bej/kanban/board/service/TaskServiceImpl.java
@@ -1,6 +1,7 @@
 package com.niit.bej.kanban.board.service;
 
 import com.niit.bej.kanban.board.model.TaskDTO;
+import com.niit.bej.kanban.board.repository.TaskRepository;
 import org.springframework.data.mongodb.core.messaging.Task;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +9,13 @@ import java.util.List;
 import java.util.Optional;
 @Service
 public class TaskServiceImpl implements TaskService {
+
+    private final TaskRepository taskRepository;
+
+    public TaskServiceImpl(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
     @Override
     public List<Task> getAllTasks() {
         return null;
