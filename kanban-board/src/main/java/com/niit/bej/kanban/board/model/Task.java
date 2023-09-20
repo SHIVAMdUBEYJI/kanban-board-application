b6cc8c2e65dce1963f88f0221683e9fbfe3f29a1
Saranyan.E <saran201496@gmail.com>
2023-09-20 14:56:11 +0530

    added the fields for Task.java

---
+++ kanban-board/src/main/java/com/niit/bej/kanban/board/model/Task.java
@@ -0,0 +1,9 @@
+package com.niit.bej.kanban.board.model;
+
+public class Task {
+    private Long id;
+    private String title;
+    private String description;
+    private String color;
+    private TaskStatus status;
+}
