e5162b0dedde0d1b996bfebe42518f61377b14d7
Saranyan.E <saran201496@gmail.com>
2023-09-23 14:41:52 +0530

    added the constructors for kanban.component.ts

--- kanban-board-frontend/src/app/components/kanban-board/kanban/kanban.component.ts
+++ kanban-board-frontend/src/app/components/kanban-board/kanban/kanban.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Task} from "../../../models/task/task.model";
+import {Kanban} from "../../../models/kanban/kanban.model";
+import {KanbanServiceService} from "../../../services/kanban-service.service";
+import {TaskServiceService} from "../../../services/task-service.service";
+import {ActivatedRoute} from "@angular/router";
+import {MatDialog} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-kanban',
   templateUrl: './kanban.component.html',
   styleUrls: ['./kanban.component.css']
 })
-export class KanbanComponent {
+export class KanbanComponent implements OnInit{
+
+	kanban: Kanban;
+	todos: Task[] =[];
+	inProgress:Task[] =[];
+	dones :Task[]=[];
+
+	constructor(private kanbanService:KanbanServiceService,
+				private taskService:TaskServiceService,
+				private route:ActivatedRoute,
+				private dialog:MatDialog) {
+	}
+	ngOnInit(): void {
+	}
 
 }
