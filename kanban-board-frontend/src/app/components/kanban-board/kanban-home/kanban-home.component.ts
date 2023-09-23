998073ecd527fa07c8b3af8775aa2ad991b1fad9
Saranyan.E <saran201496@gmail.com>
2023-09-23 11:48:06 +0530

    added the openDialogForNewKanban(); retrieveAllKanbanBoards()

--- kanban-board-frontend/src/app/components/kanban-board/kanban-home/kanban-home.component.ts
+++ kanban-board-frontend/src/app/components/kanban-board/kanban-home/kanban-home.component.ts
@@ -1,21 +1,35 @@
 import {Component, OnInit} from '@angular/core';
 import {Kanban} from "../../../models/kanban/kanban.model";
 import {KanbanServiceService} from "../../../services/kanban-service.service";
-import {MatDialog} from "@angular/material/dialog";
+import {MatDialog, MatDialogConfig} from "@angular/material/dialog";
+import {KanbanDialogComponent} from "../kanban-dialog/kanban-dialog.component";
 
 @Component({
-  selector: 'app-kanban-home',
-  templateUrl: './kanban-home.component.html',
-  styleUrls: ['./kanban-home.component.css']
+	selector: 'app-kanban-home', templateUrl: './kanban-home.component.html', styleUrls: ['./kanban-home.component.css']
 })
-export class KanbanHomeComponent implements OnInit{
+export class KanbanHomeComponent implements OnInit {
 
 	kanbanList: Kanban[] | undefined;
 
-	constructor(private kanbanService:KanbanServiceService,
-				private dialog: MatDialog) {
-	}
-	ngOnInit(): void {
+	constructor(private kanbanService: KanbanServiceService, private dialog: MatDialog) {
 	}
 
+	ngOnInit(): void {
+		this.retrieveAllKanbanBoards();
+	}
+
+	openDialogForNewKanban(): void {
+		const dialogConfig = new MatDialogConfig();
+		dialogConfig.autoFocus = true;
+		dialogConfig.data = {
+			kanban: new Kanban()
+		};
+		this.dialog.open(KanbanDialogComponent, dialogConfig)
+	}
+
+	private retrieveAllKanbanBoards(): void {
+		this.kanbanService.retrieveAllKanbanBoards().subscribe(response => {
+			this.kanbanList = response;
+		})
+	}
 }
