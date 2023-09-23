c36eeac52424a92fc6bed091ffd9e1c816f013bb
Saranyan.E <saran201496@gmail.com>
2023-09-23 17:48:57 +0530

    created the updateTask() ; getTaskById()

--- kanban-board-frontend/src/app/services/task-service.service.ts
+++ kanban-board-frontend/src/app/services/task-service.service.ts
@@ -1,9 +1,26 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {environment} from "../../environments/environment";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Task} from "../models/task/task.model";
+import {Observable} from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class TaskServiceService {
 
-  constructor() { }
+	private kanbanAppUrl = environment.kanbanAppUrl
+
+	constructor(private httpClient: HttpClient) {
+	}
+
+	updateTask(task: Task): Observable<Task> {
+		let headers = new HttpHeaders({'Content-Type': 'application/json'});
+		let options = {headers: headers};
+		return this.httpClient.put<Task>(this.kanbanAppUrl + '/tasks/' + task.id, task, options);
+	}
+
+	getTaskById(id: string): Observable<Task> {
+		return this.httpClient.get<Task>(this.kanbanAppUrl + '/tasks/' + id);
+	}
 }
