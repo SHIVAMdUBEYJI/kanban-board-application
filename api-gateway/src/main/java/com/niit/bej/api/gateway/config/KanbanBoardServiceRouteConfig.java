0270cbec67e589113f0d770a2e7b010a72c86788
Saranyan.E <saran201496@gmail.com>
2023-09-24 12:35:29 +0530

    corsConfigurationSource() added in KanbanBoardServiceRouteConfig.java

--- api-gateway/src/main/java/com/niit/bej/api/gateway/config/KanbanBoardServiceRouteConfig.java
+++ api-gateway/src/main/java/com/niit/bej/api/gateway/config/KanbanBoardServiceRouteConfig.java
@@ -6,6 +6,9 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @LoadBalancerClient(name = "kanban-board-service")
@@ -17,4 +20,17 @@ public class KanbanBoardServiceRouteConfig {
         return routeLocatorBuilder.routes().route(routePredicate -> routePredicate.path("/api/v1/**").uri("lb://kanban-board-service")).build();
     }
 
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return source;
+    }
 }
