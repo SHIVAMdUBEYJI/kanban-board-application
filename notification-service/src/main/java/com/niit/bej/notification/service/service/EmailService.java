8089337d9aa5b4c8cf080c3ed55fd5804b67e27f
shivamdubey <dubeyjishivam63@gmail.com>
2023-10-04 10:51:49 +0530

    created EmailService.java

---
+++ notification-service/src/main/java/com/niit/bej/notification/service/service/EmailService.java
@@ -0,0 +1,23 @@
+package com.niit.bej.notification.service.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    private JavaMailSender javaMailSender;
+    public boolean sendEmail(String toEmail,String subject,String body){
+        SimpleMailMessage message =new SimpleMailMessage();
+        message.setFrom("KanbanInfo.@gmail.com");
+        message.setTo(toEmail);
+        message.setSubject(subject);
+        message.setText(body);
+        javaMailSender.send(message);
+        System.out.println(message);
+        System.out.println("Email sent successfully to: " + toEmail);
+        return true;
+    }
+}
